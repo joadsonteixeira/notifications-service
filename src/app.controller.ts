8ce39ab2764fdc83eb13452864d2d89d15eee72a
joadsonteixeira <joadsonchagas@aluno.uema.br>
2022-12-13 21:10:37 -0300

    Fim primeira aula Ignite Lab

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,30 @@
 import { Controller, Get } from '@nestjs/common';
+import { Body, Post } from '@nestjs/common/decorators';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
+import { CreateNotificationBody } from './create-notification-body';
 
-@Controller()
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { content, recipientId, category } = body;
+
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content: content,
+        category: category,
+        recipientId: recipientId
+      }
+    })
   }
 }
