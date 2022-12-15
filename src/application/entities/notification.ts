2c31516ef44eda0b621d756a5227d322b5b8ab53
joadsonteixeira <joadsonchagas@aluno.uema.br>
2022-12-14 22:17:49 -0300

    Testes Content Notification Send-Notification

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -1,6 +1,9 @@
+import { Replace } from "src/helpers/Replace";
+import { Content } from "./content";
+
 export interface NotificationProps {
   recipientId: string;
-  content: string;
+  content: Content;
   category: string;
   readAt?: Date | null;
   createdAt: Date;
@@ -9,8 +12,11 @@ export interface NotificationProps {
 export class Notification {
   private props: NotificationProps;
 
-  constructor(props: NotificationProps) {
-    this.props = props;
+  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
   }
 
   public get recipientId(): string {
@@ -21,11 +27,11 @@ export class Notification {
     this.props.recipientId = recipientId;
   }
 
-  public get content(): string {
+  public get content(): Content {
     return this.props.content;
   }
 
-  public set content(content: string) {
+  public set content(content: Content) {
     this.props.content = content;
   }
 
